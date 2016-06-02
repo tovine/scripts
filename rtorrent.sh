e3228a85018d17c972bc7cada61afd2c92d08525
Torbjørn Viem Ness <torbjorn@viem-it.no>
2016-06-02 21:19:51 +0200

    Updated RTorrent script with 'start in background' functionality

--- rtorrent.sh
+++ rtorrent.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+if [ "$1" == "background" ]; then
+	BACKGROUND=true
+fi
 if ! screen -list | grep -q "rtorrent"; then
-	screen -mS rtorrent /usr/bin/rtorrent
+	# Fix terminal keys interfering with rTorrent
+	stty stop undef;
+	stty start undef;
+	if $BACKGROUND; then
+		echo "Starting in background"
+		screen -dmS rtorrent /usr/bin/rtorrent
+	else
+		screen -mS rtorrent /usr/bin/rtorrent
+	fi
 	echo "RTorrent not running, started new session..."
 else
 	screen -rx rtorrent
